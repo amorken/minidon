b549951e6630b308be2e5a3b561651164c9e109a
Anders Daljord Morken <amorken@gmail.com>
2026-05-21 20:35:11 +0000

    Implement status DTO types in model package

--- internal/model/status.go
+++ internal/model/status.go
@@ -12,6 +12,34 @@
 //   - MediaAttachments (PreviewURL, Type)
 //   - Tags (Name)
 //   - Reblog (recursive, depth 1)
-//
-// TODO: define Status, Account, MediaAttachment, Tag structs with JSON tags.
 package model
+
+import "time"
+
+type Status struct {
+	ID               string            `json:"id"`
+	URI              string            `json:"uri"`
+	URL              string            `json:"url"`
+	CreatedAt        time.Time         `json:"created_at"`
+	Content          string            `json:"content"`
+	Language         string            `json:"language"`
+	Account          Account           `json:"account"`
+	MediaAttachments []MediaAttachment `json:"media_attachments"`
+	Tags             []Tag             `json:"tags"`
+	Reblog           *Status           `json:"reblog"`
+}
+
+type Account struct {
+	Acct        string `json:"acct"`
+	DisplayName string `json:"display_name"`
+	Avatar      string `json:"avatar"`
+}
+
+type MediaAttachment struct {
+	PreviewURL string `json:"preview_url"`
+	Type       string `json:"type"`
+}
+
+type Tag struct {
+	Name string `json:"name"`
+}
